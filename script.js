a66394dadd9604024416b7921cfb87755f4774a9
Agustin Brizuela <agustinapb33@gmail.com>
2023-07-27 13:04:43 -0300

    PF

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
-let productos = [
-  { id: 2, nombre: "Iphone 12", categoria: "telefono", precio: 600, stock: 3, rutaImagen: "iphone12.jpg" },
-  { id: 3, nombre: "Iphone 13", categoria: "telefono", precio: 760, stock: 10, rutaImagen: "iphone13.jpg" },
-  { id: 5, nombre: "Iphone 14", categoria: "telefono", precio: 850, stock: 10, rutaImagen: "iphone14.jpg" },
-  { id: 7, nombre: "Iphone 14 Pro", categoria: "telefono", precio: 950, stock: 10, rutaImagen: "iphone14pro.jpg" },
-  { id: 9, nombre: "Funda (todos los modelos)", categoria: "accesorio", precio: 10, stock: 10, rutaImagen: "funda.jpg" },
-  { id: 12, nombre: "Protector vidrio templado", categoria: "accesorio", precio: 5, stock: 10, rutaImagen: "protector.jpg" },
-]
-let carrito = []
-let carritoJSON = JSON.parse(localStorage.getItem("carrito")) || [];
+function programaPricipal() {
 
-if (carritoJSON) {
-  carrito = carritoJSON
-}
+  let productos = [  
+    { id: 2, nombre: "Iphone 12", categoria: "telefono", precio: 600, stock: 3, rutaImagen: "iphone12.jpg" },
+    { id: 3, nombre: "Iphone 13", categoria: "telefono", precio: 760, stock: 10, rutaImagen: "iphone13.jpg" },
+    { id: 5, nombre: "Iphone 14", categoria: "telefono", precio: 850, stock: 10, rutaImagen: "iphone14.jpg" },
+    { id: 7, nombre: "Iphone 14 Pro", categoria: "telefono", precio: 950, stock: 10, rutaImagen: "iphone14pro.jpg" },
+    { id: 9, nombre: "Funda (todos los modelos)", categoria: "accesorio", precio: 10, stock: 10, rutaImagen: "funda.jpg" },
+    { id: 12, nombre: "Protector vidrio templado", categoria: "accesorio", precio: 5, stock: 10, rutaImagen: "protector.jpg" },
+  ]
+console.log(productos)
+
+/* 
+ let productos = []
+ const urlLocal = './json.json'
+ fetch(urlLocal)
+  .then(response => response.json())
+  .then(data =>{
+    productos = data.productos
+    console.log(productos)
+  }
+  )
+  */
+let carritoJSON = JSON.parse(localStorage.getItem("carrito"));
+let carrito = carritoJSON ? carritoJSON : []
 let contenedor = document.getElementById("contenedor")
 
-renderizar(productos)
-renderizarCarrito(carritoJSON)
-function renderizar(arrayDeElementos) {
+renderizar(productos, contenedor, carrito)
+renderizarCarrito(carrito)
+
+let botonFinalizarCompra = document.getElementById("terminarCompra")
+botonFinalizarCompra.addEventListener("click", () => finalizarCompra(carrito))
+
+}
+
+programaPricipal()
+
+function renderizar(arrayDeElementos, contenedor, carrito) {
 
   contenedor.innerHTML = ""
   arrayDeElementos.forEach(producto => {
@@ -36,9 +55,57 @@ function renderizar(arrayDeElementos) {
   `
     contenedor.appendChild(tarjetaProducto)
     let botonAgregarAlCarrito = document.getElementById(producto.id)
-    botonAgregarAlCarrito.addEventListener("click", agregarAlCarrito)
+    botonAgregarAlCarrito.addEventListener("click", () => agregarAlCarrito(arrayDeElementos, producto.id, carrito))
 
   })
+}
+
+function agregarAlCarrito(arrayDeElementos, id, carrito) {
+  
+  let productoBuscado = arrayDeElementos.find(producto => producto.id === id)
+  let productoCarrito = carrito.findIndex(producto => producto.id === id)
+  
+  if (productoCarrito !== -1) {
+    carrito[productoCarrito].unidad++
+    carrito[productoCarrito].subtotal = carrito[productoCarrito].unidad * carrito[productoCarrito].precioUni
+  }else{
+    carrito.push({
+      id: productoBuscado.id,
+      nombre: productoBuscado.nombre,
+      precioUni: productoBuscado.precio,
+      unidad: 1,
+      subtotal: productoBuscado.precio
+    })
+  }
+  lanzarTosti()
+  localStorage.setItem("carrito", JSON.stringify(carrito))
+  renderizarCarrito(carrito)
+}
+
+function renderizarCarrito(carritoJSON) {
+  let carritoFisico = document.getElementById("carrito")
+  carritoFisico.innerHTML= ""
+  carritoJSON.forEach(producto => {
+    carritoFisico.innerHTML += `<p>${producto.nombre} ${producto.precioUni} ${producto.unidad} ${producto.subtotal}</p>\n`
+  })
+}
+
+function finalizarCompra(carrito) {
+  let carritoFisico = document.getElementById("carrito")
+  carritoFisico.innerHTML = ""
+  localStorage.removeItem("carrito")
+  carrito = []
+  lanzarAlert()
+}
+
+let botonCarrito = document.getElementById("botonCarrito")
+botonCarrito.addEventListener("click", mostrarOcultar)
+
+function mostrarOcultar() {
+  let padreContenedor = document.getElementById("productos")
+  let carrito = document.getElementById("contenedorCarrito")
+  padreContenedor.classList.toggle("oculto")
+  carrito.classList.toggle("oculto")
 }
 
 let input = document.getElementById("miInput")
@@ -61,34 +128,19 @@ function filtrarYRenderizarPorCategoria(e) {
   renderizar(elementosFiltrados)
 }
 
-let botonCarrito = document.getElementById("botonCarrito")
-botonCarrito.addEventListener("click", mostrarOcultar)
-
-function agregarAlCarrito(e) {
-  let productoBuscado = productos.find(producto => producto.id === Number(e.target.id))
-  carrito.push({
-    id: productoBuscado.id,
-    nombre: productoBuscado.nombre,
-    precio: productoBuscado.precio
-  })
-  renderizarCarrito(carritoJSON)
-  localStorage.setItem("carrito", JSON.stringify(carrito))
-
-}
-
-
-function renderizarCarrito(carritoJSON) {
-  let carritoFisico = document.getElementById("carrito")
-
-  carritoJSON.forEach(producto => {
-    carritoFisico.innerHTML += `<p>${producto.nombre} ${producto.precio}</p>\n`
+function lanzarAlert() {
+  Swal.fire({
+    title: 'Haz finalizado la compra',
+    icon: 'success',
+    confirmButtonText: 'Cool',
+    duration: 3000
   })
 }
 
-function mostrarOcultar() {
-  let padreContenedor = document.getElementById("productos")
-  let carrito = document.getElementById("carrito")
-  padreContenedor.classList.toggle("oculto")
-  carrito.classList.toggle("oculto")
+function lanzarTosti() {
+  Toastify({
+    text: "Producto agregado al carrito",
+    className: "info",
+    duration: 2000
+  }).showToast();
 }
-
